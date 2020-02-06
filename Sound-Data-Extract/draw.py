e76d234b6b6570923c84c7ff0ef108d6d15db4b5
CTFang's Macbook <andy89923@gmail.com>
2020-02-06 16:39:50 +0800

    Maximum Point Record

--- Sound-Data-Extract/draw.py
+++ Sound-Data-Extract/draw.py
@@ -2,13 +2,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def draw(y1, y2):
+	fig = plt.figure()
+	ax = fig.subplots()
+
+	ax.plot(np.linspace(0, 1, len(yy1)), yy1, 'b')
+	ax.plot(np.linspace(0, 1, len(yy2)), yy2, 'r')
+
+	plt.show()
+
+def getMax(lis):
+	poi = -1
+	num = 0
+	for i in range(len(lis)):
+		if lis[i] > num:
+			num = lis[i]
+			poi = i
+	return poi
+
+
 pre_dir = "./data/NewData VOL2/"
 
 num = 0
 yy1 = list()
 yy2 = list()
 
-data = open(pre_dir + 'dual_0l_pin0.txt')
+data = open(pre_dir + 'dual_-90l_pin0.txt')
 for i in data:
 	now = i.split(',')
 	num = num + 1
@@ -17,18 +36,14 @@ for i in data:
 	yy1.append(float(now[0]))
 	yy2.append(float(now[1]))
 
-	if num >= 333516:
-		break
+	if num >= 303516:
+		poi1 = getMax(yy1)
+		poi2 = getMax(yy2)
 
+		print(poi1, poi2, poi2 - poi1)
+		draw(yy1, yy2)
+		
+		num = 0
+		yy1.clear()
+		yy2.clear()
 
-xx = np.linspace(0, 1, num)
-
-print(num)
-
-fig = plt.figure()
-ax = fig.subplots()
-
-ax.plot(xx, yy1, 'b')
-ax.plot(xx, yy2, 'r')
-
-plt.show()
